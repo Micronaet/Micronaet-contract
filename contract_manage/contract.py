4c827c076c03e90bf880d63deabf36bce316ce57
nicothebrush <nicola.riolini@gmail.com>
2015-07-13 11:57:11 +0200

    remove assert

--- contract_manage/contract.py
+++ contract_manage/contract.py
@@ -223,7 +223,7 @@ class account_analytic_expense(osv.osv):
                     ('department_id', '=', department_id),
                     ], context=context)
                 if account_ids: # TODO
-                    assert len(account_ids) == 1, 'Key broken prot-ledge-dept!'
+                    #assert len(account_ids) == 1, 'Key broken prot-ledge-dept!'
                     self.write(cr, uid, account_ids, data, context=context)
                     item_id = account_ids[0]
                 else:
