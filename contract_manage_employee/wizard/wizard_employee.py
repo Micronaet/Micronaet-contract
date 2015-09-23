064972efa8412851156683a09b9b5f9b77fd20e1
nicothebrush <nicola.riolini@gmail.com>
2015-09-23 11:01:37 +0200

    End import procedure start debug wizard

--- contract_manage_employee/wizard/wizard_employee.py
+++ contract_manage_employee/wizard/wizard_employee.py
@@ -41,7 +41,9 @@ class hr_employee_force_hour(osv.osv_memory):
     _name = 'hr.employee.force.hour.wizard'
     _description = 'Employee force hour cost'
     
+    # --------------------
     # Schedule operations:
+    # --------------------
     def schedule_importation_cost(self, cr, uid, path='~/etl/employee', 
             bof='cost', separator=';', context=None):
         ''' Loop on cost folder searching file that start with bof
@@ -49,22 +51,47 @@ class hr_employee_force_hour(osv.osv_memory):
         from os import listdir
         from os.path import isfile, join
 
+        path = os.path.expanduser(path)
         cost_file = [
             filename for filename in listdir(path) if 
                     isfile(join(path, filename)) and filename.startswith(bof)]
 
         for filename in cost_file:
-            # Load:
-            self.load_one_cost(cr, uid, path, filename, separator, 
-                context=context)
-                
-            # Import:
-                
-    
+            try:
+                # Load:
+                self.load_one_cost(cr, uid, path, filename, separator, 
+                    context=context)
+                    
+                # Import:
+                # parse date
+                filename. 
+                file_date = os.path.splitext(filename)[-4:]
+                from_month = file_date[:2]
+                from_year = file_date[2:]
+                # Next value
+                if from_month = '12':
+                    next_month = '01'
+                    next_year = '%02d' % int(from_year) + 1
+                else:    
+                    next_month = '%02d' % int(from_month) + 1
+                    next_year = from_year
+
+                from_date = '01-%02d-%02d' % (from_month, from_year)
+                to_date = '01-%02d-%02d' % (to_month, to_year)
+
+                self.import_one_cost(cr, uid, from_date=from_date, 
+                    to_date=to_date, context=context)
+            except:
+                #raise osv.except_osv(
+                #    _('Error!'), _('No file found for import: %s' % fullname))
+                _logger.error('No correct file format: %s' % filename)
         return True
 
+    # --------
     # Utility:        
-    def load_one_cost(self, cr, uid, path, filename, separator, context=None):
+    # --------
+    def load_one_cost(self, cr, uid, path, filename, separator, 
+            from_wizard=False, context=None):
         ''' Import one file 
         '''
         # Utility: function for procedure:
@@ -77,123 +104,99 @@ class hr_employee_force_hour(osv.osv_memory):
         def format_date(value):
             return value
 
-        def format_float(value):
+        def format_float(value):        
             try:
                 value = value.replace(',', '.')
                 return float(value)
             except:
                 return 0.0
 
-        join(os.path.expanduser(path), filename)
-        return True
-    
-    # def import_one_cost(self, cr, uid, context=None):    
-    
-
-    # Button events:
-    def load_button(self, cr, uid, ids, context=None):
-        ''' Load all active employee and his product, create if not present
-            After open view with the list
-        '''
-        # ---------------------------------------------------------------------
-        # Import file parameters (TODO parametrize):
-        # ---------------------------------------------------------------------
-        path = '~/etl/servizi/employee/'
-        separator = ';'
-        bof = 'costi' # begin of file
-        tot_col = 5
-
-        self.load_one_cost(cr, uid, path, filename, separator, context=context)
-        
-
-        #wiz_proxy = self.browse(cr, uid, ids)[0]
-        domain = [] # Domain depend on mode
+        tot_col = 5 # TODO change if file will be extended
+        fullname = join(os.path.expanduser(path), filename)
+        domain = []
         force_cost = {}
-
-        # -----------------------------------------------------------------
-        #                   Load from file bofMMAA.csv
-        # -----------------------------------------------------------------
-        i = 0
+       
+        # Load from file employee:
+        employee_pool = self.pool.get('hr.employee')            
+        item_ids = [] # employee list
         
-        # Loop on all file in folder (that start with bof):
-        cost_file = [
-            join(os.path.expanduser(filepath), f)
-                for f in listdir(filepath) if 
-                    isfile(join(filepath, f)) and f.startswith(bof)]
+        # Check file present (for wizard but used alse for sched.)?
+        try: 
+            f = open(os.path.expanduser(fullname), 'rb')
+        except:
+            raise osv.except_osv(
+                _('Error!'), _('No file found for import: %s' % fullname))
 
-        for filename in cost_file:
-            try:
-                # Load from file employee:
-                employee_pool = self.pool.get('hr.employee')            
-                item_ids = [] # employee list
-                # check file present?
-                try: 
-                    f = open(os.path.expanduser(filename), 'rb')
-                except:
-                    raise osv.except_osv(
-                        _('Error!'), 
-                        _('No file found for import: %s' % filename))
+        i = 0
+        for line in f:
+            i += 1
+            line = line.strip()
+            if not line:
+                _logger.warning('Empty line (jumped): %s' % i)
+                continue
+            record = line.split(separator)
+            if len(record) != tot_col:
+                _logger.error('Record different format: %s (col.: %s)' % (
+                    tot_col))
+                continue
+            
+            code = format_string(record[0], False)
+            name = format_string(record[1]).title()
+            surname = format_string(record[2]).title()
+            cost = format_float(record[3])
+            total = format_float(record[4])
+            
+            # TODO search first for code
+            employee_ids = employee_pool.search(cr, uid, [
+                '|',
+                ('name', '=', "%s %s" % (name, surname)),
+                ('name', '=', "%s %s" % (surname, name)),
+                ], context=context)
+
+            # TODO update code if found only one    
+            if len(employee_ids) == 1:
+                employee_id = employee_ids[0]
+                if employee_id in item_ids:
+                    _logger.error('Double in CSV file: %s %s' % (
+                        surname, name))
+                else:
+                    item_ids.append(employee_id)
+                    force_cost[employee_id] = cost # save cost
+            elif len(employee_ids) > 1:
+                _logger.error('Fount more employee: %s %s' % (
+                    surname, name))                   
+            else:
+                _logger.error('Employee not found: %s %s' % (
+                    surname, name))
+            
+        domain.append(('id', 'in', item_ids))
                 
-
-                for line in f:
-                    i += 1
-                    line = line.strip()
-                    if not line:
-                        _logger.warning('Empty line (jumped): %s' % i)
-                        continue
-                    record = line.split(separator)
-                    if len(record) != tot_col:
-                        _logger.error('Record different format: %s (col.: %s)' % (
-                            tot_col))
-                        continue
-                    
-                    code = format_string(record[0], False)
-                    name = format_string(record[1]).title()
-                    surname = format_string(record[2]).title()
-                    cost = format_float(record[3])
-                    total = format_float(record[4])
-                    
-                    employee_ids = employee_pool.search(cr, uid, [
-                        '|',
-                        ('name', '=', "%s %s" % (name, surname)),
-                        ('name', '=', "%s %s" % (surname, name)),
-                        ], context=context)
-                    if len(employee_ids) == 1:
-                        employee_id = employee_ids[0]
-                        if employee_id in item_ids:
-                            _logger.error('Double in CSV file: %s %s' % (
-                                surname, name))
-                        else:
-                            item_ids.append(employee_id)
-                            force_cost[employee_id] = cost # save cost
-                    elif len(employee_ids) > 1:
-                        _logger.error('Fount more employee: %s %s' % (
-                            surname, name))                   
-                    else:
-                        _logger.error('Employee not found: %s %s' % (
-                            surname, name))
-                    
-                domain.append(('id', 'in', item_ids))
-
+        # Load in object for check:
         self.pool.get('hr.employee.hour.cost').load_all_employee(
             cr, uid, domain, force_cost, context=context)
             
-        return {
-            'view_type': 'form',
-            'view_mode': 'tree,form',
-            'res_model': 'hr.employee.hour.cost', # object linked to the view
-            #'views': [(view_id, 'form')],
-            'view_id': False,
-            'type': 'ir.actions.act_window',
-            #'target': 'new',
-            #'res_id': res_id, # ID selected
-            }
-
-    def force_button(self, cr, uid, ids, context=None):
-        ''' Force button update records
-        ''' 
-        wiz_proxy = self.browse(cr, uid, ids)[0]
-
+        # Open view:    
+        if from_wizard:
+            return {
+                'view_type': 'form',
+                'view_mode': 'tree,form',
+                'res_model': 'hr.employee.hour.cost', # object linked to the view
+                #'views': [(view_id, 'form')],
+                'view_id': False,
+                'type': 'ir.actions.act_window',
+                #'target': 'new',
+                #'res_id': res_id, # ID selected
+                }
+        else:
+            return True
+    
+    def import_one_cost(self, cr, uid, name='', from_date=False, to_date=False,
+            from_wizard=False, context=None):
+        ''' Import previous loaded list of employee costs
+            name: for log description
+            from_date: from date update analytic line
+            from_wizard: for return operation 
+        '''
         # Pools used:
         cost_pool = self.pool.get('hr.employee.hour.cost')
         product_pool = self.pool.get('product.product')
@@ -206,9 +209,10 @@ class hr_employee_force_hour(osv.osv_memory):
         # ---------------
         # Note: Logged before for get ID
         update_log_id = log_pool.log_operation(
-            cr, uid, wiz_proxy.name, wiz_proxy.from_date, context=context)
+            cr, uid, name, from_date, context=context)
 
         cost_ids = cost_pool.search(cr, uid, [], context=context)
+
         # TODO filter new = old?
         current_ids = cost_pool.search(cr, uid, [], context=context)
         for cost in cost_pool.browse(cr, uid, cost_ids, context=context):
@@ -232,10 +236,15 @@ class hr_employee_force_hour(osv.osv_memory):
                 # Update analytic lines save log operation parent:
                 # ------------------------------------------------
                 #if abs(cost.hour_cost - cost.hour_cost_new) >= 0.01: # approx
-                line_ids = line_pool.search(cr, uid, [
+                domain = [
                     ('user_id', '=', cost.employee_id.user_id.id),
-                    ('date', '>=', wiz_proxy.from_date),
-                    ], context=context)
+                    ('date', '>=', from_date),
+                    ]
+                if to_date: # for schedule update only
+                    domain.append(
+                        ('date', '<', to_date),
+                        )    
+                line_ids = line_pool.search(cr, uid, domain, context=context)
                     
                 # loop for total calculation:
                 for line in line_pool.browse(
@@ -247,21 +256,54 @@ class hr_employee_force_hour(osv.osv_memory):
                         'product_id': cost.product_id.id, 
                         }, context=context)
             except:
-                _logger.error('Emploee update: %s' % cost.employee_id.name)
+                _logger.error('Employee update: %s' % cost.employee_id.name)
                 _logger.error(sys.exc_info(), )
-                current_ids.remove(cost.id) # not update (remain)
+                # TODO log error for reinsert (not remove)
+                #current_ids.remove(cost.id) # not update (remain)
                 continue
 
         # -------------------------------------
         # Remove all record (update correctly):
         # -------------------------------------
         cost_pool.unlink(cr, uid, current_ids, context=context)
-        return {'type': 'ir.actions.act_window_close'}
+        if from_wizard:
+            return {'type': 'ir.actions.act_window_close'}
+        else:    
+            return True
+
+    # --------------
+    # Button events:
+    # --------------
+    def load_button(self, cr, uid, ids, context=None):
+        ''' Load all active employee and his product, create if not present
+            After open view with the list
+        '''
+        # ----------------------
+        # Import file parameters
+        # ----------------------
+        # TODO load parameter from scheduled importation
+        path = '~/etl/servizi/employee/'
+        separator = ';'
+        bof = 'costi' # begin of file
+
+        return self.load_one_cost(cr, uid, path, filename, separator, 
+            from_wizard=True, context=context)
+
+    def force_button(self, cr, uid, ids, context=None):
+        ''' Force button update records
+        ''' 
+        wiz_proxy = self.browse(cr, uid, ids)[0]
+        
+        # NOTE: no to_date from wizard:
+        return self.import_one_cost(cr, uid, wiz_proxy.name, 
+            wiz_proxy.from_date, False, from_wizard=True, context=context)
 
     _columns = {
         'name': fields.char('Description', size=80),
         'from_date': fields.date('From date', 
             help='Choose the date, every intervent from that date take costs'),
+        #'to_date': fields.date('To date', 
+        #    help='Choose the date, every intervent from that date take costs'),
         'operation': fields.selection([
             ('load', 'Load current'),
             ('absence', 'Force update'),
@@ -275,6 +317,7 @@ class hr_employee_force_hour(osv.osv_memory):
         
     _defaults = {
         'from_date': lambda *x: datetime.now().strftime('%Y-%m-%d'),
+        #'to_date': lambda *x: datetime.now().strftime('%Y-%m-%d'),
         'operation': lambda *a: 'load',
         'mode': lambda *a: 'file',
         }
