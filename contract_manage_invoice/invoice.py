1281ae26954e0d2961fe096a206487d76d5e0b42
nicothebrush <nicola.riolini@gmail.com>
2015-10-01 22:50:48 +0200

    Renamed all fields

--- contract_manage_invoice/invoice.py
+++ contract_manage_invoice/invoice.py
@@ -95,144 +95,130 @@ class account_analytic_account(osv.osv):
         for csv_file in invoice_file:
             lines = csv.reader(
                 open(csv_file, 'rb'), delimiter=delimiter)
-            i = -header
-            tot_colonne = 0
+
+            # Reset element for import:
+            cols = 0
             fattura_precedente = ''
+            i = -header
+            for line in lines:
+                i += 1
+                if not cols:
+                    cols = len(line)
+                    _logger.info(_('Total column found: %s') % cols)
 
-        for line in lines:
-            if counter['tot']<0:  # jump n lines of header 
-               counter['tot']+=1
-            else: 
-               if not tot_colonne:
-                  tot_colonne=len(line)
-                  print "Colonne presenti: %d"%(tot_colonne)
-               if len(line): # jump empty lines
-                   if tot_colonne == len(line): # tot colums equal to column first line                   
-                       counter['tot']+=1 
-                       print counter['tot'],") ",
-                       # campi rilevati dall'importazione CSV:
-                       csv_id=0
-                       sigla = prepare(line[csv_id]).upper()                # 1. FT 
-                       csv_id+=1
-                       numero = prepare(line[csv_id])                       # 2. numero fattura
-                       csv_id+=1
-                       data = prepare_date_ISO(line[csv_id])                # 3. data fattura
-                       csv_id+=1
-                       codice = prepare(line[csv_id]).upper()               # 4. codice articolo
-                       csv_id+=1
-                       descrizione = prepare(line[csv_id])                  # 5. descrizione articolo
-                       csv_id+=1
-                       quantita = prepare_float(line[csv_id])               # 6. quantità
-                       csv_id+=1
-                       importo_riga = prepare_float(line[csv_id])           # 7. importo totale riga
-                       csv_id+=1
-                       importo = prepare_float(line[csv_id])                # 8. importo totale fattura
-                       csv_id+=1
-                       commessa_testata = prepare(line[csv_id])             # 9. commessa per la fattura (tutta)
-                       csv_id+=1
-                       data_periodo = prepare_date_ISO(line[csv_id])        # 10. fine periodo di validità, es.: 20120331
-                       csv_id+=1
-                       numero_riga = prepare(line[csv_id])                  # 11. numero riga all'interno della fattura (per l'ID)
-                       csv_id+=1
-                       vid_agg_1 = prepare(line[csv_id])                    # 12. riga: anno commessa, es.: 2012
-                       csv_id+=1
-                       vid_agg_2 = prepare(line[csv_id])                    # 13. riga: numero. es.: 097
-                       csv_id+=1
-                       vid_agg_3 = prepare(line[csv_id])                    # 14. riga: giorno / mese, es.: 23/12
-                       csv_id+=1 
-                       vid_agg_4 = prepare(line[csv_id])                    # 15. riga: anno, es.: 2011
+                if i <= 0:  # jump n lines of header
+                    continue
 
-                       # campi calcolati:
-                       if not data[:4]:
-                           error = "ERRORE: data non presente, riga: %s"%(counter['tot'])
-                           print error
-                           file_log.write("%s"%(error,))
-                           continue
-                       ref = "%s-%s-(%s)"%(sigla,numero,data[:4])                           # ID fattura
-                       ref_line = "%s-R%s"%(ref,numero_riga)      # ID riga
+                if len(line) and cols == len(line): 
+                   document = prepare(line[0]).upper() # Type, ex.: FT 
+                   number = prepare(line[1]) # Number of invoice
+                   date = prepare_date_ISO(line[2]) # Invoice date
+                   item_code = prepare(line[3]).upper() # Code item
+                   item_description = prepare(line[4]) # Description item
+                   quantity = prepare_float(line[5]) # Quantity
+                   amount_row = prepare_float(line[6]) # Total line
+                   amount = prepare_float(line[7]) # Total invoice
+                   contract = prepare(line[8]) # Header: contract
+                   period = prepare_date_ISO(line[9]) # End val. 20120331
+                   sequence = prepare(line[10]) # Num. row in invoice
+                   addendum1 = prepare(line[11]) # Row: contract year ex. 2012
+                   addendum2 = prepare(line[12]) # Row: Number ex.: 097
+                   addendum3 = prepare(line[13]) # Row: day/month ex.: 23/12
+                   addendum4 = prepare(line[14]) # Row: year ex. 2011
+                   series = prepare(line[15]) # Series ex. 2
+
+                   # Computed
+                   if not date[:4]:
+                       log_error.append(_('Date not present, row: %s') % i)
+                       _logger.error(log_error[-1])
+                       continue
                        
-                       if sigla in ("FT","NF"):
-                          segno = +1
-                       elif sigla in ("NC", "FF"):
-                          segno = -1
-                       else:
-                          error = "[ERR] Sigla documento non trovata:", sigla
-                          print error
-                          file_log.write("%s"%(error,))
+                   # Key ref:
+                   ref = "%s/%s-%s-(%s)" % (document, series, number, date[:4])                   
+                   ref_line = "%s-R%s" % (ref, sequence) # row
+                   
+                   if document in ("FT","NF"):
+                      segno = +1
+                   elif document in ("NC", "FF"):
+                      segno = -1
+                   else:
+                      error = "[ERR] Sigla documento non trovata:", sigla
+                      print error
+                      file_log.write("%s"%(error,))
 
-                       if descrizione: # if line to analytic account so there's the description
-                           import_type = "L"    # L for single (L)ine
-                           amount = importo_riga * segno
-                           ref_id = ref_line
-                       else:    
-                           import_type = "I"    # I for all (I)nvoice
-                           amount = importo * segno
-                           ref_id = ref
+                   if item_description: # if line to analytic account so there's the description
+                       import_type = "L"    # L for single (L)ine
+                       amount = amount_row * segno
+                       ref_id = ref_line
+                   else:    
+                       import_type = "I"    # I for all (I)nvoice
+                       amount = amount * segno
+                       ref_id = ref
+                   
+                   if not amount: # TODO per le linee come viene messo l'importo?***********************************************************
+                       error = "[WARN] Amount not present:", ref, ref_line, "su commessa", contract, "importo:", amount
+                       if verbose: print error
+                       file_log.write("%s"%(error,))
+       
+                       continue # jump line
+
+                   unit_amount = 1.0 # TODO vedere se è il caso di mettere il totale elementi o tenere 1 come per fattura
+
+                   period = period or data    # prendo la data fattura se non è indicata
+                   
+
+                   # Ricerca conto analitico:
+                   # TODO IMPORTANTE: vedere poi per le sottovoci come comporsi: eventualmente commessa + numero sottovoce
+                   account_ids = sock.execute(dbname, uid, pwd, 'account.analytic.account', 'search', [('code', '=', contract),]) # TODO tolto ('parent_id','=',False)
+                   
+                   if not account_ids: 
+                       error = "[ERR] Conto analitico non trovato:", contract 
+                       print error
+                       file_log.write("%s"%(error,))
+
+                   else: # TODO segnalare errore se len(account_ids) è >1
+                       # Creazione voce giornale analitico
+                       line_id = sock.execute(dbname, uid, pwd, 'account.analytic.line', 'search', [('ref', '=', ref_id),('import_type', '=', import_type)])   # for I: ref=FT-2-12345 for L: ref=FT-2-12345-1
                        
-                       if not amount: # TODO per le linee come viene messo l'importo?***********************************************************
-                           error = "[WARN] Amount not present:", ref, ref_line, "su commessa", commessa_testata, "importo:", amount
-                           if verbose: print error
-                           file_log.write("%s"%(error,))
-           
-                           continue # jump line
+                       data_account = {
+                                     'name': "%s %s"%(contract, ref_id) ,#'2010001 FERIE',
+                                     'import_type': import_type,
+                                     #'code': False,
+                                     #'user_id': [419, 'Moletta Giovanni'],
+                                     'general_account_id': general_account_id, #[146, '410100 merci c/acquisti '],
+                                     #'product_uom_id': False,
+                                     #'company_id': [1, u'Franternit\xe0 Servizi'],
+                                     'journal_id': journal_id, #[2, 'Timesheet Journal'],
+                                     #'currency_id': False,
+                                     #'to_invoice': [1, 'Yes (100%)'],
+                                     'amount': amount,
+                                     #'product_id': False,
+                                     'unit_amount': unit_amount, #10.5,
+                                     #'invoice_id': False,
+                                     'date': period, #'2012-07-09',
+                                     #'extra_analytic_line_timesheet_id': False,
+                                     #'amount_currency': 0.0,
+                                     'ref': ref_id,   # TODO or ref_line
+                                     #'move_id': False,
+                                     'account_id': account_ids[0], #[257, '2010001 FERIE']
+                                 }
 
-                       unit_amount = 1.0 # TODO vedere se è il caso di mettere il totale elementi o tenere 1 come per fattura
-
-                       data_periodo = data_periodo or data    # prendo la data fattura se non è indicata
-                       
-
-                       # Ricerca conto analitico:
-                       # TODO IMPORTANTE: vedere poi per le sottovoci come comporsi: eventualmente commessa + numero sottovoce
-                       account_ids = sock.execute(dbname, uid, pwd, 'account.analytic.account', 'search', [('code', '=', commessa_testata),]) # TODO tolto ('parent_id','=',False)
-                       
-                       if not account_ids: 
-                           error = "[ERR] Conto analitico non trovato:", commessa_testata 
-                           print error
-                           file_log.write("%s"%(error,))
-
-                       else: # TODO segnalare errore se len(account_ids) è >1
-                           # Creazione voce giornale analitico
-                           line_id = sock.execute(dbname, uid, pwd, 'account.analytic.line', 'search', [('ref', '=', ref_id),('import_type', '=', import_type)])   # for I: ref=FT-2-12345 for L: ref=FT-2-12345-1
-                           
-                           data_account = {
-                                         'name': "%s %s"%(commessa_testata, ref_id) ,#'2010001 FERIE',
-                                         'import_type': import_type,
-                                         #'code': False,
-                                         #'user_id': [419, 'Moletta Giovanni'],
-                                         'general_account_id': general_account_id, #[146, '410100 merci c/acquisti '],
-                                         #'product_uom_id': False,
-                                         #'company_id': [1, u'Franternit\xe0 Servizi'],
-                                         'journal_id': journal_id, #[2, 'Timesheet Journal'],
-                                         #'currency_id': False,
-                                         #'to_invoice': [1, 'Yes (100%)'],
-                                         'amount': amount,
-                                         #'product_id': False,
-                                         'unit_amount': unit_amount, #10.5,
-                                         #'invoice_id': False,
-                                         'date': data_periodo, #'2012-07-09',
-                                         #'extra_analytic_line_timesheet_id': False,
-                                         #'amount_currency': 0.0,
-                                         'ref': ref_id,   # TODO or ref_line
-                                         #'move_id': False,
-                                         'account_id': account_ids[0], #[257, '2010001 FERIE']
-                                     }
-
-                           if line_id: # UPDATE:
-                               try:
-                                   item_mod = sock.execute(dbname, uid, pwd, 'account.analytic.line', 'write', line_id, data_account) 
-                                   if verbose: print "[INFO] Account already exist, updated:", ref_id, "su commessa", commessa_testata, "importo:", amount
-                               except:
-                                   error = "[ERR] modified", ref_id, "su commessa", commessa_testata, "importo:", amount
-                                   print error
-                                   file_log.write("%s"%(error,))
-                           else: # CREATE
-                              try:
-                                  create_id = sock.execute(dbname, uid, pwd, 'account.analytic.line', 'create', data_account) 
-                                  if verbose: print "[INFO] Account create: ", ref_id, "su commessa", commessa_testata, "importo:", amount
-                              except:
-                                  error = "[ERR] modified", ref_id, "su commessa", commessa_testata, "importo:", amount
-                                  print error
-                                  file_log.write("%s"%(error,))
+                       if line_id: # UPDATE:
+                           try:
+                               item_mod = sock.execute(dbname, uid, pwd, 'account.analytic.line', 'write', line_id, data_account) 
+                               if verbose: print "[INFO] Account already exist, updated:", ref_id, "su commessa", contract, "importo:", amount
+                           except:
+                               error = "[ERR] modified", ref_id, "su commessa", contract, "importo:", amount
+                               print error
+                               file_log.write("%s"%(error,))
+                       else: # CREATE
+                          try:
+                              create_id = sock.execute(dbname, uid, pwd, 'account.analytic.line', 'create', data_account) 
+                              if verbose: print "[INFO] Account create: ", ref_id, "su commessa", contract, "importo:", amount
+                          except:
+                              error = "[ERR] modified", ref_id, "su commessa", contract, "importo:", amount
+                              print error
+                              file_log.write("%s"%(error,))
 
 
 
