df14763f7db98e09e6bd6c0657e47417d930815b
nicothebrush <nicola.riolini@gmail.com>
2015-11-02 10:28:02 +0100

    remove dep for error (and pep8)

--- base_import_fraternita/partner.py
+++ base_import_fraternita/partner.py
@@ -30,45 +30,42 @@
 from osv import osv, fields
 
 class res_partner_extra_fields(osv.osv):
-    _name='res.partner'
     _inherit ='res.partner'
 
     _columns = {
-               'extra_phone': fields.text('Extra phone'),
-               'is_employee': fields.boolean('Is employee', required=False),
-    }
+       'extra_phone': fields.text('Extra phone'),
+       'is_employee': fields.boolean('Is employee', required=False),
+       }
 res_partner_extra_fields()
 
 class res_partner_address_extra_fields(osv.osv):
-    _name='res.partner.address'
     _inherit ='res.partner.address'
 
     _columns = {
-               'extra_phone': fields.text('Extra phone'),
-    }
+        'extra_phone': fields.text('Extra phone'),
+        }
 res_partner_address_extra_fields()
 
 class hr_employee_extra_fields(osv.osv):
-    _name='hr.employee'
     _inherit ='hr.employee'
 
     _columns = {
-               'import':fields.boolean('Import', required=False),
-               'birth_place': fields.char('Birth place', size=80, required=False, readonly=False), 
-               
-               # settore
-               # stato
-               # TODO mettere i campi nella view!!
-               'date_recruitment': fields.date('Date recruitment'), # data_assunzione
-               'date_retired': fields.date('Date retired'), # data dimissione
-               'curricula': fields.text('Curricula'), 
-               'patent_type': fields.char('Patent type', size=5, required=False, readonly=False),  # patente
-               # mansione               
-               # orario di lavoro
-               # tipo contratto 
-               'date_end_contract': fields.date('End contract'),  # data fine contratto
-               # > department_id (esiste)
-               # > contract_ids (esiste
-    }
+        'import':fields.boolean('Import', required=False),
+        'birth_place': fields.char('Birth place', size=80, required=False, readonly=False), 
+       
+        # settore
+        # stato
+        # TODO mettere i campi nella view!!
+        'date_recruitment': fields.date('Date recruitment'), # data_assunzione
+        'date_retired': fields.date('Date retired'), # data dimissione
+        'curricula': fields.text('Curricula'), 
+        'patent_type': fields.char('Patent type', size=5, required=False, readonly=False),  # patente
+        # mansione               
+        # orario di lavoro
+        # tipo contratto 
+        'date_end_contract': fields.date('End contract'),  # data fine contratto
+        # > department_id (esiste)
+        # > contract_ids (esiste
+        }
 hr_employee_extra_fields()
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
